b0907022e0a92ce302a31a2dfe34cfae055d4133
韦文彬 <weiwb@yidingyun.com>
2024-08-28 17:32:38 +0800

    feat: WebSocketConfig

--- src/main/java/com/lylbp/manager/websocket/config/WebSocketConfig.java
+++ src/main/java/com/lylbp/manager/websocket/config/WebSocketConfig.java
@@ -1,28 +1,28 @@
-//package com.lylbp.manager.websocket.config;
-//
-//import com.lylbp.manager.websocket.client.MyWebSocketClient;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.socket.server.standard.ServerEndpointExporter;
-//
-//import java.net.URI;
-//import java.util.HashMap;
-//import java.util.Map;
-//
-///**
-// * WebSocketConfig
-// *
-// * @Author weiwenbin
-// * @Date 2020/7/17 下午4:52
-// */
-//@Configuration
-//public class WebSocketConfig {
-//    @Bean
-//    public ServerEndpointExporter serverEndpointExporter() {
-//        return new ServerEndpointExporter();
-//    }
-//
-//
+package com.lylbp.manager.websocket.config;
+
+import com.lylbp.manager.websocket.client.MyWebSocketClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * WebSocketConfig
+ *
+ * @Author weiwenbin
+ * @Date 2020/7/17 下午4:52
+ */
+@Configuration
+public class WebSocketConfig {
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+
+
 //    /**
 //     * 后端模拟websocket客户端
 //     *
@@ -35,4 +35,4 @@
 //        URI uri = URI.create(wsUrl);
 //        return new MyWebSocketClient(uri, httpHeaders);
 //    }
-//}
+}
